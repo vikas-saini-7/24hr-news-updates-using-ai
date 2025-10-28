d42f21a6264add72656fa42864448fa7c26bd2d8
vikas-saini-7 <vikas289888@gmail.com>
2025-10-28 22:46:54 +0530

    fix : removed one homepage section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <Hero />
       </div>
 
-      <HowItWorksSection />
+      {/* <HowItWorksSection /> */}
       <QualitiesSection />
       <TestimonialSection />
       <CTASection />
